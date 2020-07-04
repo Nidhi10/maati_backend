c0d420ac44c19e9e0d48755ce6b2dd2e6dc3cb83
Nidhi101 <nidhi105@gmail.com>
2020-07-04 21:24:16 +0000

    Offo

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const wss = new WebSocket.Server({ port: process.env.PORT });
 
 wss.on('connection', function connection(ws) {
 	ws.on('message', function incoming(data) {
+	  console.warn(data);
 		wss.clients.forEach(function each(client) {
 			if (client.readyState === WebSocket.OPEN) {
 				client.send(data);
